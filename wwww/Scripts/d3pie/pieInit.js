1323cfa5a6981dedb198da768938d46f61979ca4
Kakao <kakaosb@yandex.ru>
2017-05-21 14:01:37 +0300

    style done, added contacts and language of page

---
+++ wwww/Scripts/d3pie/pieInit.js
@@ -0,0 +1,89 @@
+﻿function CreatePie(countA, countB, countC) {
+    var pie = new d3pie("pieChart", {
+        "header": {
+            "title": {
+                "text": "Count Letters",
+                "fontSize": 24,                
+                "color": "#f5deb3"
+            },
+            "subtitle": {
+                "color": "#f5deb3",
+                "fontSize": 12,
+                "font": "open sans"
+            },
+            "titleSubtitlePadding": 9
+        },
+        "footer": {
+            "color": "#f5deb3",
+            "fontSize": 10,
+            "font": "open sans",
+            "location": "bottom-left"
+        },
+        "size": {
+            "canvasWidth": 590,
+            "pieOuterRadius": "90%"
+        },
+        "data": {
+            "sortOrder": "value-desc",
+            "content": [
+                {
+                    "label": "A",
+                    "value": countA,
+                    "color": "#990000"
+                },
+                {
+                    "label": "B",
+                    "value": countB,
+                    "color": "#660000"
+                },
+                {
+                    "label": "C",
+                    "value": countC,
+                    "color": "#330000"
+                }
+            ]
+        },
+        "labels": {
+            "outer": {
+                "pieDistance": 32
+            },
+            "inner": {
+                "hideWhenLessThanPercentage": 3
+            },
+            "mainLabel": {
+                "fontSize": 30,
+                "color": "#f5deb3"
+            },
+            "percentage": {
+                "color": "#fff",
+                "decimalPlaces": 0,
+                "fontSize": 30
+            },
+            "value": {
+                "color": "#fff",
+                "fontSize": 20
+            },
+            "lines": {
+                "enabled": true
+            },
+            "truncation": {
+                "enabled": true
+            }
+        },
+        "effects": {
+            "pullOutSegmentOnClick": {
+                "effect": "linear",
+                "speed": 400,
+                "size": 8
+            }
+        },
+        "misc": {
+            "gradient": {
+                "enabled": true,
+                "percentage": 100
+            }
+        }
+    });
+
+    pieee = pie;
+};
